5f1f099588b7c59d02ad8bd0ece7e59586d870ff
Bogdan Constantinescu <bogstar007@yahoo.com>
2022-04-05 22:07:48 +0300

    trigger action

--- dummy.go
+++ dummy.go
@@ -4,6 +4,7 @@ package dummy
 import "github.com/pkg/errors"
 
 // NewStackedError returns an error with stack.
+// Pretty dummy function.
 func NewStackedError(err error) error {
 	return errors.WithStack(err)
 }
