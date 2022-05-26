1a75939b8dc8e4217899d576fe0e38266f92dcff
bogcon <bog.con.bc@gmail.com>
2022-05-26 11:22:47 +0300

    test

--- setup_dockers_deb.sh
+++ setup_dockers_deb.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-#!/bin/sh -eux
-
 echo ">>> Install deps"
+apt-get update
 apt-get install -y --no-install-recommends \
-	docker-ce \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    jq
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y --no-install-recommends \
+    docker-ce \
     docker-ce-cli \
-	curl \
-	jq \
-; \
+    containerd.io \
+    docker-compose-plugin
 
 network=$(docker inspect --format '{{json .NetworkSettings.Networks}}' `hostname` | jq -r 'keys[0]')
 echo "network = ${network}"
