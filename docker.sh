e670dcfbde89800f1d6fa2bbef23a0bf58e69d5e
bogcon <bog.con.bc@gmail.com>
2022-05-26 10:46:01 +0300

    test

--- docker.sh
+++ docker.sh
@@ -8,11 +8,21 @@ echo "network = ${network}"
 
 echo ">>> Run Consul Docker Image"
 docker pull -q consul:1.12.0
-docker run -d --name=some-consul -p 8500:8500 -e CONSUL_BIND_INTERFACE=eth0 consul:1.12.0
+docker run -d \
+    --name=some-consul \
+    -p 8500:8500 \
+    --network "${network}" \
+    -e CONSUL_BIND_INTERFACE=eth0 \
+    consul:1.12.0
 
 echo ">>> Run Etcd Docker Image"
 docker pull -q quay.io/coreos/etcd:v3.5.3
-docker run -d --name=some-etcd -p 2379:2379 quay.io/coreos/etcd:v3.5.3 /usr/local/bin/etcd -advertise-client-urls http://some-etcd:2379 -listen-client-urls http://0.0.0.0:2379
+docker run -d \
+    --name=some-etcd \
+    -p 2379:2379 \
+    --network "${network}" \
+    quay.io/coreos/etcd:v3.5.3 \
+    /usr/local/bin/etcd -advertise-client-urls http://some-etcd:2379 -listen-client-urls http://0.0.0.0:2379
 
 echo ">>> Show Running Docker Containers"
 sleep 10
